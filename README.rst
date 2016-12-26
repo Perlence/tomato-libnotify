0bdc3ac648a66d8be7ddf55dced9bfa1e34f767a
Sviatoslav Abakumov <abakumov.sviatoslav@progforce.com>
2016-12-26 22:50:12 +0300

    Add README.rst

---
+++ README.rst
@@ -0,0 +1,20 @@
+tomato-libnotify
+================
+
+`The Pomodoro Technique® <http://cirillocompany.de/pages/pomodoro-technique/>`_ timer for Linux based on libnotify.
+
+
+Requirements
+------------
+
+- python (>= 3.4)
+- python3-gi
+- libnotify-dev
+
+
+Installation
+------------
+
+.. code-block:: sh
+
+    pip install 'git+https://github.com/Perlence/tomato-libnotify#egg=tomato-libnotify'
--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
-# with open('README.md') as fp:
-#     README = fp.read()
+with open('README.rst') as fp:
+    README = fp.read()
 
 setup(
     name='tomato-libnotify',
     version='0.1',
     author='Sviatoslav Abakumov',
     author_email='dust.harvesting@gmail.com',
-    description='...',
-    # long_description=README,
+    description='The Pomodoro Technique® timer for Linux based on libnotify',
+    long_description=README,
     url='https://github.com/Perlence/tomato-libnotify',
     download_url='https://github.com/Perlence/tomato-libnotify/archive/master.zip',
     packages=find_packages('.'),
@@ -26,12 +26,20 @@ setup(
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
-        'Environment :: Console',
+        'Environment :: X11 Applications',
+        'Environment :: X11 Applications :: GTK',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved',
         'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
         'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Utilities',
     ]
 )
